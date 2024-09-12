92e40eabbd6d57550d83d76ba8d259c23320cdd6
dav1dct <lindahwatih215@gmail.com>
2024-09-12 12:53:00 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,6 +11,13 @@ app.get('/about', (req, res) => {
 app.get('/contact', (req, res) => {
     res.send('Contact us at 1234567890')
 });
+
+//middleware
+app.use('*', (req,res) =>{
+    res.status(404);
+    res.send("404, Halaman tidak ditemukan");
+});
+
 app.listen(port, () => {
     console.log('Server started on port 3000');
 });
