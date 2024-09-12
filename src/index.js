e1372a6cfb25c06fdf10b3c3c2d424414a9d549b
dav1dct <lindahwatih215@gmail.com>
2024-09-12 12:38:16 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,12 @@ const port = 3000
 app.get('/', (req, res) => {
     res.send('Hello World')
 });
-
+app.get('/about', (req, res) => {
+    res.send('Halaman tentang perusahaan')
+});
+app.get('/contact', (req, res) => {
+    res.send('Contact us at 1234567890')
+});
 app.listen(port, () => {
     console.log('Server started on port 3000');
 });
