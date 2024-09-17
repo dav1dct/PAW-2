775c6082d8b0a2261c446bce3b9626da38562f91
dav1dct <121503738+dav1dct@users.noreply.github.com>
2024-09-17 15:07:25 +0700

    Update index.js

--- P3/routes/index.js
+++ P3/routes/index.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const mhsRouter = require('./mahasiswa');
 
 router.use('/mahasiswa',mhsRouter);
-router.use((req, res))
+router.use((req, res, next) => {
+    res.status(404).send('404 NOT FOUND');
+})
 module.exports = router;
