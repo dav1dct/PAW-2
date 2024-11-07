0b308f077e17d2069eaec34461a67daff4fadf20
dav1dct <lindahwatih215@gmail.com>
2024-11-07 13:05:05 +0700

    angular

---
+++ ANGULAR1/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { PostCreateComponent } from './post-create/post-create.component';
+import { FormsModule } from '@angular/forms';
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, PostCreateComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'ANGULAR1';
+}
