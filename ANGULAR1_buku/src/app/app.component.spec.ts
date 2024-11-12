a6e94e8bfe87a57b2be62d0c6ba582f59fd04526
dav1dct <lindahwatih215@gmail.com>
2024-11-12 15:10:42 +0700

    ANGULAR

---
+++ ANGULAR1_buku/src/app/app.component.spec.ts
@@ -0,0 +1,35 @@
+import { TestBed } from '@angular/core/testing';
+import { RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        RouterModule.forRoot([])
+      ],
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'ANGULAR1_buku'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('ANGULAR1_buku');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ANGULAR1_buku');
+  });
+});
---
+++ FrontEnd_ANGULAR1/src/app/admin/admin.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { DashboardComponent } from '../dashboard/dashboard.component';
+
+@Component({
+  selector: 'app-admin',
+  standalone: true,
+  imports: [DashboardComponent],
+  templateUrl: './admin.component.html',
+  styleUrl: './admin.component.css'
+})
+export class AdminComponent {
+
+}
