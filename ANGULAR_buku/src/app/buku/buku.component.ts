118b52f57964fc67f85162aa7afe14af53e1b845
dav1dct <lindahwatih215@gmail.com>
2024-11-14 12:50:16 +0700

    1

---
+++ ANGULAR_buku/src/app/buku/buku.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-buku',
+  templateUrl: './buku.component.html',
+  styleUrl: './buku.component.css'
+})
+export class BukuComponent {
+
+}
