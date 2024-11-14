652acbe323dd9fbc65b08ac6a5501f384704808f
dav1dct <lindahwatih215@gmail.com>
2024-11-14 13:03:22 +0700

    buku.component

--- ANGULAR_buku/src/app/buku/buku.component.ts
+++ ANGULAR_buku/src/app/buku/buku.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-buku',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrl: './buku.component.css'
 })
 export class BukuComponent {
-
+  simpanBuku(form : NgForm){
+    console.log("Pengujian Klik")
+  }
 }
