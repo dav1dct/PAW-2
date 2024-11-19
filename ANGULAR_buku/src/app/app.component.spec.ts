3bf0f552ee99dac533c136023a9e3d4929e22484
dav1dct <lindahwatih215@gmail.com>
2024-11-19 13:48:35 +0700

    1

--- ANGULAR_buku/src/app/app.component.spec.ts
+++ ANGULAR_buku/src/app/app.component.spec.ts
@@ -1,16 +1,10 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterModule.forRoot([])
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [AppComponent],
     }).compileComponents();
   });
 
@@ -20,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'prjbuku'`, () => {
+  it(`should have the 'prjfrontend' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('prjbuku');
+    expect(app.title).toEqual('prjfrontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, prjbuku');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, prjfrontend');
   });
 });
